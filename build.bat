32a74f1fa487dded56f3362f7c90a518f8d51987
abnrk <adudowas@gmail.com>
2025-07-12 23:30:17 +0100

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,11 @@
+@echo off
+curl -O https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js
+FOR /F "tokens=*" %%g IN ('findstr "version:" user.js') do (SET version=%%g)
+FOR /F "tokens=*" %%g IN ('findstr "version:" okayfox.js') do (SET version_copy=%%g)
+set /a version=%version:~11,-1%
+set /a version_copy=%version_copy:~11,-1%
+if %version_copy% neq %version% (
+  @echo okayfox.js is outdated! (%version_copy% vs %version%)
+)
+busybox diff -u user.js okayfox.js > okayfox.patch
+busybox patch user.js okayfox.patch
