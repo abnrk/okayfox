44445f7e641e88ad595f99fb43e1be26461535bf
abnrk <adudowas@gmail.com>
2025-07-13 13:33:55 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,6 +4,8 @@ FOR /F "tokens=*" %%g IN ('findstr "version:" user.js') do (SET version=%%g)
 FOR /F "tokens=*" %%g IN ('findstr "version:" okayfox.js') do (SET version_copy=%%g)
 set /a version=%version:~11,-1%
 set /a version_copy=%version_copy:~11,-1%
+@echo user.js version: %version%
+@echo okayfox.js version: %version%
 if %version_copy% neq %version% (
   @echo okayfox.js is outdated! (%version_copy% vs %version%)
 )
